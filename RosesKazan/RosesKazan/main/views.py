f344065511d23c875fcc81ee30cb62ba79de4205
Amir <70170580+murtanli@users.noreply.github.com>
2024-04-01 00:19:39 +0300

    front_end_main v.1.7

--- RosesKazan/RosesKazan/main/views.py
+++ RosesKazan/RosesKazan/main/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .forms import *
 
+
 def main_page(request):
+    flowers = ["цветок 1", "цветок 2", "цветок 3", "цветок 4", "цветок 5", "цветок 6", "цветок 7", "цветок 8", "цветок 9", "цветок 10", "цветок 11", "цветок 12"]
+    roses = ["роза 1", "роза 2", "роза 3", "роза 4", "роза 5", "роза 6", "роза 7", "роза 8", "роза 9", "роза 10", "роза 11", "роза 12"]
     if request.method == 'GET':
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            return render(request, "main.html", {'result': query})
+            return render(request, "stocks.html", {'result': query, 'title': 'Главная страница'})
         else:
             form = SearchForm()
-    return render(request, "main.html", {'form': form})
-
+    return render(request, "stocks.html", {'form': form, 'title': 'Главная страница', "flowers": flowers, "roses": roses})
